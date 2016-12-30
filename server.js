e1ae0467ed9bc3fbb8485b3210fdfbe0e1679cd4
Kjell-Morten <post@kjellmorten.no>
2016-12-30 13:24:15 +0100

    Start node.js server on great.start()

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const Integreat = require('./index')
 
-const great = new Integreat()
+const config = {
+  port: 3000,
+  db: {
+    url: process.env.GR8_COUCH_URL,
+    db: process.env.GR8_COUCH_DB,
+    key: process.env.GR8_COUCH_KEY,
+    password: process.env.GR8_COUCH_PASSWORD
+  }
+}
+
+const great = new Integreat(config)
 
 great.loadDefaults()
 // great.loadSourceDefsFromDb()
 
 great.start()
-.then(() => {
-  console.log('Integreat is running')
+.then((server) => {
+  console.log('Integreat is running on port %d.', server.address().port)
 })
