00ec432daee13a1c2765b5f2c6610459cc2c2d5d
Kjell-Morten <post@kjellmorten.no>
2016-12-31 17:17:29 +0100

    Setup simple debugging

--- server.js
+++ server.js
@@ -15,6 +15,22 @@ const great = new Integreat(config)
 great.loadDefaults()
 // great.loadSourceDefsFromDb()
 
+const sourceFns = {
+  sourcetype: 'json',
+  fetch: {endpoint: 'http://api.feednstatus.com/1.0/accounts'},
+  item: {
+    type: 'account',
+    attributes: {
+      id: {path: 'id'},
+      name: {path: 'attributes.name'}
+    }
+  },
+  sync: {
+    schedule: 60
+  }
+}
+great.setSource('fnsaccount', sourceFns)
+
 great.start()
 .then((server) => {
   console.log('Integreat is running on port %d.', server.address().port)
