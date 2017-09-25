21ef8a4ea5e0202dd8450a176f250ece1523394a
Kjell-Morten <post@kjellmorten.no>
2017-09-25 14:25:30 +0200

    Remove example config

--- examples/auths/couchdb.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  id: 'couchdb',
-  strategy: 'couchdb',
-  options: {
-    uri: process.env.GR8_COUCH_URL,
-    key: process.env.GR8_COUCH_KEY,
-    secret: process.env.GR8_COUCH_PASSWORD
-  }
-}
--- examples/types/account.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  id: 'account',
-  source: 'accounts',
-  attributes: {
-    username: {type: 'string'}
-  }
-}
