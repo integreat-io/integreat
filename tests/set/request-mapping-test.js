0a30b4ede21eb1120c2bd3cbd1e561419ed60d79
Kjell-Morten <post@kjellmorten.no>
2020-01-09 12:41:32 +0100

    Upgrade to map-transform 0.3.7

--- tests/set/request-mapping-test.js
+++ tests/set/request-mapping-test.js
@@ -72,3 +72,53 @@ test('should set data with request mapping', async (t) => {
 
   nock.restore()
 })
+
+test('should set data with reverse transform', async (t) => {
+  const requestData = {
+    content: {
+      items: [entry1Mapped],
+      footnote: '',
+      meta: '{"datatype":"entry"}'
+    }
+  }
+  nock('http://some.api')
+    .put('/entries/ent1', requestData)
+    .reply(201, { id: 'ent1', ok: true, rev: '1-12345' })
+  const action = {
+    type: 'SET',
+    payload: { type: 'entry', data: entry1Item },
+    meta: { ident: { root: true } }
+  }
+  const resources = {
+    adapters: { json: json() },
+    transformers: {
+      stringify: Object.assign(
+        (value) => JSON.parse(value),
+        { rev: (value) => JSON.stringify(value) }
+      )
+    }
+  }
+  const requestMapping = {
+    'content.items[]': 'data',
+    'content.footnote': { const: '' },
+    'content.meta': { path: 'params.type', transform: 'stringify', sub: 'datatype' }
+  }
+  const defs = {
+    schemas: [entrySchema],
+    services: [{
+      ...entriesService,
+      endpoints: [{
+        requestMapping,
+        options: { uri: '/{id}' }
+      }]
+    }],
+    mappings: [entriesMapping]
+  }
+
+  const great = integreat(defs, resources)
+  const ret = await great.dispatch(action)
+
+  t.is(ret.status, 'ok', ret.error)
+
+  nock.restore()
+})
