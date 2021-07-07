67d59e42fb12524762dd31bbeec2585621c4acb4
Kjell-Morten <post@kjellmorten.no>
2021-07-07 13:51:58 +0200

    v0.8.0-alpha.27

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ export { default as transformers } from './transformers'
 export { default as middleware } from './middleware'
 
 export default {
-  version: '0.8.0-alpha.26',
+  version: '0.8.0-alpha.27',
   create,
   createQueue,
   mergeResources,
