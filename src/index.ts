a75f320c3761ad5cf57ec04442a55e9b34cb80f6
Kjell-Morten <post@kjellmorten.no>
2021-09-08 20:45:55 +0200

    v0.8.0-alpha.41

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ export { default as transformers } from './transformers'
 export { default as middleware } from './middleware'
 
 export default {
-  version: '0.8.0-alpha.40',
+  version: '0.8.0-alpha.41',
   create,
   mergeResources,
 }
