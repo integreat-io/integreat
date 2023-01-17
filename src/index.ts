6a43ec49a1b01c4653a9c271b260280debd1ea8d
Kjell-Morten <post@kjellmorten.no>
2023-01-17 12:40:44 +0100

    v0.8.0-beta.21

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ export { default as middleware } from './middleware/index.js'
 export { default as createAction } from './utils/createAction.js'
 
 export default {
-  version: '0.8.0-beta.20',
+  version: '0.8.0-beta.21',
   create,
   mergeDefinitions,
   mergeResources,
