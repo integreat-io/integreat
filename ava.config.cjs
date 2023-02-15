6873c2b4eb49d3293b98231a21c82000bf62a268
Kjell-Morten <post@kjellmorten.no>
2023-02-15 23:53:02 +0100

    Don't watch .md files

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   extensions: { ts: 'module' },
   nodeArguments: ['--loader=ts-node/esm', '--no-warnings'],
-  ignoredByWatcher: ['{coverage,dist,media}/**'],
+  ignoredByWatcher: ['{coverage,dist,media}/**', '**/*.md'],
   files: ['src/**/*.test.ts'],
 }
