f34f09d69161e6c570f01aa5f5146d1924163776
Kjell-Morten <post@kjellmorten.no>
2023-02-26 22:52:49 +0100

    v0.8.0-beta.30

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,29 @@ import create from './create.js'
 import mergeDefinitions from './utils/mergeDefinitions.js'
 import mergeResources from './utils/mergeResources.js'
 
-// export { Transformer, MapDefinition } from 'map-transform'
-// export {
-//   Action,
-//   Payload,
-//   Meta,
-//   Response,
-//   Headers,
-//   Ident,
-//   Dispatch,
-//   TypedData,
-//   Reference,
-//   Transporter,
-//   Connection,
-//   Middleware,
-//   ActionHandler,
-//   ActionHandlerResources,
-//   HandlerDispatch,
-//   SetProgress,
-//   JobDef,
-//   Job,
-// } from './types.js'
-// export { Authenticator, AuthDef, ServiceDef } from './service/types.js'
-// export { Instance, Definitions, Resources } from './create.js'
+export { Transformer, MapDefinition } from 'map-transform'
+export {
+  Action,
+  Payload,
+  Meta,
+  Response,
+  Headers,
+  Ident,
+  Dispatch,
+  TypedData,
+  Reference,
+  Transporter,
+  Connection,
+  Middleware,
+  ActionHandler,
+  ActionHandlerResources,
+  HandlerDispatch,
+  SetProgress,
+  JobDef,
+  Job,
+} from './types.js'
+export { Authenticator, AuthDef, ServiceDef } from './service/types.js'
+export { Instance, Definitions, Resources } from './create.js'
 
 export { default as authenticators } from './authenticators/index.js'
 export { default as mutations } from './mutations/index.js'
@@ -33,7 +33,7 @@ export { default as middleware } from './middleware/index.js'
 export { default as createAction } from './utils/createAction.js'
 
 export default {
-  version: '0.8.0-beta.29',
+  version: '0.8.0-beta.30',
   create,
   mergeDefinitions,
   mergeResources,
