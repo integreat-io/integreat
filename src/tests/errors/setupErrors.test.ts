84707688f7fe5ff1ae535426ada89e919bc94b48
Kjell-Morten <post@kjellmorten.no>
2023-04-09 19:54:33 +0200

    Make sure create() throws when map-transform throws

---
+++ src/tests/errors/setupErrors.test.ts
@@ -0,0 +1,35 @@
+import test from 'ava'
+import defs from '../helpers/defs/index.js'
+import resources from '../helpers/resources/index.js'
+
+import Integreat from '../../index.js'
+
+// Tests
+
+test('should throw on unknown transformer', (t) => {
+  const resourcesWithoutTransformers = { ...resources, transformers: {} }
+
+  const error = t.throws(() =>
+    Integreat.create(defs, resourcesWithoutTransformers)
+  )
+
+  t.true(error instanceof Error)
+  t.is(
+    error?.message,
+    "Transform operator was given the unknown transformer id 'shouldHaveAuthor'"
+  )
+})
+
+test('should throw on unknown mutations', (t) => {
+  const defsWithoutMutations = { ...defs, mutations: {} }
+
+  const error = t.throws(() =>
+    Integreat.create(defsWithoutMutations, resources)
+  )
+
+  t.true(error instanceof Error)
+  t.is(
+    error?.message,
+    "Failed to apply pipeline 'exchange:json'. Unknown pipeline"
+  )
+})
