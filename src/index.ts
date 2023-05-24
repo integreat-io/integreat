600f8e5ec37465c320872c7c872fad659df76f80
Kjell-Morten <post@kjellmorten.no>
2023-05-24 12:57:41 +0200

    v0.8.0-rc.5

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ export { default as middleware } from './middleware/index.js'
 export { createAction } from './utils/action.js'
 
 export default {
-  version: '0.8.0-rc.4',
+  version: '0.8.0-rc.5',
   create,
   mergeDefinitions,
   mergeResources,
