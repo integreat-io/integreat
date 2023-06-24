3e4dc3277b5d7e772ffca342e79afd5193d0e2a7
Kjell-Morten <post@kjellmorten.no>
2023-06-24 17:57:51 +0200

    v0.8.2

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ export * as adapters from './adapters/index.js'
 export { createAction } from './utils/action.js'
 
 export default {
-  version: '0.8.1',
+  version: '0.8.2',
   create,
   mergeDefinitions,
   mergeResources,
