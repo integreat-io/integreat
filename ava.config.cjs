a52e04e908f7c22a1489612db98e556dcdab33ef
Kjell-Morten <post@kjellmorten.no>
2024-02-16 11:11:08 +0100

    Update ts-dev-setup and run tests in node v18.19

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   extensions: { ts: 'module' },
-  nodeArguments: ['--loader=ts-node/esm', '--no-warnings'],
-  ignoredByWatcher: ['{coverage,dist,media}/**', '**/*.md'],
+  nodeArguments: ['--import=tsimp'],
+  watchMode: {
+    ignoreChanges: ['{coverage,dist,media,.tsimp}/**', '**/*.md'],
+  },
   files: ['src/**/*.test.ts'],
 }
