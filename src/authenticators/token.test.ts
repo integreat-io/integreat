8a212354a12057c09f2742dce5167be9a0518688
Kjell-Morten <post@kjellmorten.no>
2024-10-09 22:37:21 +0200

    Implement validate() method on token authenticator

--- src/authenticators/token.test.ts
+++ src/authenticators/token.test.ts
@@ -28,6 +28,20 @@ test('authenticate should return granted when token is set', async (t) => {
   t.deepEqual(ret, expected)
 })
 
+test('authenticate should return first token when array of tokens is given', async (t) => {
+  const options = { token: ['someToken', 'someOtherToken'] }
+  const expected = {
+    status: 'granted',
+    token: 'someToken',
+    encode: false,
+    type: undefined,
+  }
+
+  const ret = await authenticator.authenticate(options, action, dispatch)
+
+  t.deepEqual(ret, expected)
+})
+
 test('authenticate should return refused when token is not set', async (t) => {
   const options = {}
   const expected = { status: 'refused' }
@@ -49,6 +63,178 @@ test('isAuthenticated should return true when authentication is granted and toke
   const authentication = { status: 'granted', token: 'someToken' }
 
   t.true(authenticator.isAuthenticated(authentication, options, action))
+})
+
+// Test - validate
+
+test('validate should return response with the ident given by identId when token is given in the action headers', async (t) => {
+  const action = {
+    type: 'GET',
+    payload: {
+      type: 'entry',
+      headers: { Authorization: 'Bearer someToken' },
+    },
+  }
+  const options = { token: 'someToken', type: 'Bearer', identId: 'johnf' }
+  const authentication = { status: 'granted' }
+  const expected = { status: 'ok', access: { ident: { id: 'johnf' } } }
+
+  const ret = await authenticator.validate!(
+    authentication,
+    options,
+    action,
+    dispatch,
+  )
+
+  t.deepEqual(ret, expected)
+})
+
+test('validate should return response with the ident given by identId when auth header is lowercase', async (t) => {
+  const action = {
+    type: 'GET',
+    payload: {
+      type: 'entry',
+      headers: { authorization: 'Bearer someToken' },
+    },
+  }
+  const options = { token: 'someToken', type: 'Bearer', identId: 'johnf' }
+  const authentication = { status: 'granted' }
+  const expected = { status: 'ok', access: { ident: { id: 'johnf' } } }
+
+  const ret = await authenticator.validate!(
+    authentication,
+    options,
+    action,
+    dispatch,
+  )
+
+  t.deepEqual(ret, expected)
+})
+
+test('validate should return response with the ident given by identId when token is given with no type', async (t) => {
+  const action = {
+    type: 'GET',
+    payload: {
+      type: 'entry',
+      headers: { Authorization: 'someToken' },
+    },
+  }
+  const options = { token: 'someToken', identId: 'johnf' }
+  const authentication = { status: 'granted' }
+  const expected = { status: 'ok', access: { ident: { id: 'johnf' } } }
+
+  const ret = await authenticator.validate!(
+    authentication,
+    options,
+    action,
+    dispatch,
+  )
+
+  t.deepEqual(ret, expected)
+})
+
+test('validate should return response with the ident given by identId when token match one of several possible', async (t) => {
+  const action = {
+    type: 'GET',
+    payload: {
+      type: 'entry',
+      headers: { Authorization: 'Bearer otherToken' },
+    },
+  }
+  const options = {
+    token: ['someToken', 'otherToken'],
+    type: 'Bearer',
+    identId: 'johnf',
+  }
+  const authentication = { status: 'granted' }
+  const expected = { status: 'ok', access: { ident: { id: 'johnf' } } }
+
+  const ret = await authenticator.validate!(
+    authentication,
+    options,
+    action,
+    dispatch,
+  )
+
+  t.deepEqual(ret, expected)
+})
+
+test('validate should return autherror when wrong token is given in the action headers', async (t) => {
+  const action = {
+    type: 'GET',
+    payload: {
+      type: 'entry',
+      headers: { Authorization: 'Bearer wrongToken' },
+    },
+  }
+  const options = { token: 'someToken', type: 'Bearer', identId: 'johnf' }
+  const authentication = { status: 'granted' }
+  const expected = {
+    status: 'autherror',
+    error: 'Invalid credentials',
+    reason: 'invalidauth',
+  }
+
+  const ret = await authenticator.validate!(
+    authentication,
+    options,
+    action,
+    dispatch,
+  )
+
+  t.deepEqual(ret, expected)
+})
+
+test('validate should return autherror when wrong type is given in the action headers', async (t) => {
+  const action = {
+    type: 'GET',
+    payload: {
+      type: 'entry',
+      headers: { Authorization: 'Basic someToken' },
+    },
+  }
+  const options = { token: 'someToken', type: 'Bearer', identId: 'johnf' }
+  const authentication = { status: 'granted' }
+  const expected = {
+    status: 'autherror',
+    error: 'Invalid credentials',
+    reason: 'invalidauth',
+  }
+
+  const ret = await authenticator.validate!(
+    authentication,
+    options,
+    action,
+    dispatch,
+  )
+
+  t.deepEqual(ret, expected)
+})
+
+test('validate should return autherror when no auth header is given in the action headers', async (t) => {
+  const action = {
+    type: 'GET',
+    payload: {
+      type: 'entry',
+      headers: {}, // No Authorization
+    },
+  }
+  const options = { token: 'someToken', type: 'Bearer', identId: 'johnf' }
+  const authentication = { status: 'granted' }
+  const expected = {
+    status: 'noaccess',
+    error: 'Authentication required',
+    reason: 'noauth',
+  }
+
+  const ret = await authenticator.validate!(
+    authentication,
+    options,
+    action,
+    dispatch,
+  )
+
+  t.deepEqual(ret, expected)
 })
 
 // Tests -- asHttpHeaders
